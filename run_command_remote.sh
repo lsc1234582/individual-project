232a294eeb0a93115e7c03744bcf54240bbc4a5f
SiCong Li <meneerli@hotmail.com>
2018-04-17 15:39:14 +0100

    Add run commands

---
+++ run_command_remote.sh
@@ -0,0 +1,1 @@
+xvfb-run --auto-servernum --server-num=1 vrep.sh -h ../../VREPScene/MicoPush2.ttt & python ../../src/DPGAC2PEH2VEH2.py --env-name VREPPushTask --summary-dir summary --estimator-dir ./estimator --pe-h1-multiplier 16.7 --ve-h1-multiplier 16.7 --pe-h2-multiplier 12.5 --ve-h2-multiplier 12.5 --new-estimator --max-episode-length 100 --num-episodes 5000 && fg
