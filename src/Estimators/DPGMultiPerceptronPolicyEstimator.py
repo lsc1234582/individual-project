a5c4d63eee0f79c383756d72a2befb7dc7b6a2d0
SiCong Li <meneerli@hotmail.com>
2018-05-03 16:10:36 +0100

    Add imitation learning to DPGMultiPerceptronPolicyEstimator

--- src/Estimators/DPGMultiPerceptronPolicyEstimator.py
+++ src/Estimators/DPGMultiPerceptronPolicyEstimator.py
@@ -14,7 +14,7 @@ class DPGMultiPerceptronPolicyEstimator(object):
     """
 
     def __init__(self, sess, state_dim, action_dim, h_layer_shapes, learning_rate, action_bound, tau,
-            minibatch_size, scope="policy_estimator"):
+            minibatch_size, imitation_learning_rate=-1, imitation_minibatch_size=-1, scope="policy_estimator"):
         self._sess = sess
         self._state_dim = state_dim
         self._action_dim = action_dim
@@ -24,26 +24,33 @@ class DPGMultiPerceptronPolicyEstimator(object):
         self._tau = tau
         self._minibatch_size = minibatch_size
 
+        self._imitation_learning_rate = imitation_learning_rate if imitation_learning_rate != -1 else learning_rate
+        self._imitation_minibatch_size = imitation_minibatch_size if imitation_minibatch_size != -1 else\
+        minibatch_size
+
         # Actor Network
-        self._inputs, self._out, self._scaled_out = self._create_actor_network(scope)
+        self._inputs, _, self._scaled_out = self._create_actor_network(scope)
 
         self._network_params = tf.trainable_variables()
 
         # Target Network
-        self._target_inputs, self._target_out, self._target_scaled_out = self._create_actor_network(scope + "_target")
+        self._target_inputs, _, self._target_scaled_out = self._create_actor_network(scope + "_target")
 
         self._target_network_params = tf.trainable_variables()[
             len(self._network_params):]
 
+        # Make tau a placeholder so we can plug in different values within this class
+        self._tau_var = tf.placeholder(tf.float32, [], name="tau")
         with tf.name_scope(scope + "_target/"):
             # Op for periodically updating target network with online network
             # weights
             self._update_target_network_params = \
-                [self._target_network_params[i].assign(tf.multiply(self._network_params[i], self._tau) +
-                                                      tf.multiply(self._target_network_params[i], 1. - self._tau))
+                [self._target_network_params[i].assign(tf.multiply(self._network_params[i], self._tau_var) +
+                                                      tf.multiply(self._target_network_params[i], 1. - self._tau_var))
                     for i in range(len(self._target_network_params))]
 
         with tf.name_scope(scope):
+            self._actual_outputs = tf.placeholder(tf.float32, [None, self._action_dim], name="actual_outputs")
             # This gradient will be provided by the critic network
             self._action_gradient = tf.placeholder(tf.float32, [None, self._action_dim], name="action_gradient")
 
@@ -55,6 +62,11 @@ class DPGMultiPerceptronPolicyEstimator(object):
             # Optimization Op
             self._optimize = tf.train.AdamOptimizer(self._learning_rate).\
                 apply_gradients(zip(self._actor_gradients, self._network_params), name="optimize")
+
+            # MSE loss for imitation learning
+            self._imitation_loss = tflearn.mean_square(self._actual_outputs, self._scaled_out)
+            self._imitation_optimize = tf.train.AdamOptimizer(self._imitation_learning_rate).\
+                    minimize(self._imitation_loss, name="imitation_optimize")
 
         self._num_trainable_vars = len(
             self._network_params) + len(self._target_network_params)
@@ -91,6 +103,17 @@ class DPGMultiPerceptronPolicyEstimator(object):
             scaled_out = tf.multiply(out, self._action_bound, name="scaled_out")
         return inputs, out, scaled_out
 
+    def updateImitation(self, inputs, actual_outputs):
+        _, loss = self._sess.run([self._imitation_optimize, self._imitation_loss], feed_dict={
+            self._inputs: inputs,
+            self._actual_outputs: actual_outputs
+            })
+        # Also keep nn with target nn in sync
+        self._sess.run(self._update_target_network_params, feed_dict={
+            self._tau_var: 1.0
+            })
+        return loss
+
     def update(self, inputs, a_gradient):
         self._sess.run(self._optimize, feed_dict={
             self._inputs: inputs,
@@ -108,7 +131,9 @@ class DPGMultiPerceptronPolicyEstimator(object):
         })
 
     def update_target_network(self):
-        self._sess.run(self._update_target_network_params)
+        self._sess.run(self._update_target_network_params, feed_dict={
+            self._tau_var: self._tau
+            })
 
     def get_num_trainable_vars(self):
         return self._num_trainable_vars
