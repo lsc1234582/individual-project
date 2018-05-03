b3682b9e6385185e3a7434f7207c9404a91f4f93
SiCong Li <meneerli@hotmail.com>
2018-05-03 16:25:40 +0100

    [WIP] Add DPGAC2WithMultiPModelAndDemoPEH2VEH2MEH2

--- src/Agents/DPGAC2Agent.py
+++ src/Agents/DPGAC2Agent.py
@@ -485,3 +485,292 @@ class DPGAC2WithDemoAgent(AgentBase):
 
         self._max_q += max(max_qs)
 
+class DPGAC2WithMultiPModelAndDemoAgent(DPGAC2WithDemoAgent):
+    def __init__(self, sess, policy_estimator, value_estimator, model_estimator,
+            discount_factor, num_episodes, max_episode_length, minibatch_size, replay_buffer_size, actor_noise,
+            summary_writer, imitation_summary_writer, model_summary_writer,
+            estimator_dir, estimator_saver_recent, estimator_saver_best, recent_save_freq, replay_buffer_save_dir,
+            replay_buffer_save_freq, num_updates=1):
+         super().__init__(sess, policy_estimator, value_estimator,
+            discount_factor, num_episodes, max_episode_length, minibatch_size, replay_buffer_size, actor_noise,
+            summary_writer, imitation_summary_writer,
+            estimator_dir, estimator_saver_recent, estimator_saver_best, recent_save_freq, replay_buffer_save_dir,
+            replay_buffer_save_freq, num_updates)
+         self._model_summary_writer = model_summary_writer
+         self._model_estimator = model_estimator
+         self._best_action_from_model = True
+         # The episode number from which use policy solely to obtain best action
+         self._stop_best_action_from_model_episode = 100
+         # TODO: Number of episodes sampled from real environment
+
+         # Number of random actions to take
+         self._num_random_action = 50
+         # Planning horizon for model-based learning
+         self._model_plan_horizon = 5
+         # (global) model training number, used for writing summaries
+         # TODO: Make number of model training steps a tf variable in order to preserve between sessions
+         self._model_train_step = 0
+
+
+    def _trainModelFull(self, epochs):
+        """
+        TODO: Don't iterate over whole rb as the training time increases over time as rb expands
+        """
+        for i in range(epochs):
+            for exp in self._replay_buffer.sample_batch_full(self._minibatch_size):
+                current_state_batch, action_batch, reward_batch, termination_batch, next_state_batch = exp
+                model_loss = self._model_estimator.update(current_state_batch, action_batch, next_state_batch - current_state_batch)
+                self._model_summary_writer.writeSummary({
+                    "ModelTrainLoss": model_loss
+                    }, self._model_train_step)
+            self._model_train_step += 1
+
+    def _trainModel(self, steps):
+        for i in range(steps):
+            exp = self._replay_buffer.sample_batch(self._minibatch_size)
+            current_state_batch, action_batch, reward_batch, termination_batch, next_state_batch = exp
+            _, _, model_loss = self._model_estimator.update(current_state_batch, action_batch, next_state_batch - current_state_batch)
+            if i % 10 == 0:
+                self._model_summary_writer.writeSummary({
+                    "ModelTrainLoss": model_loss
+                    }, self._model_train_step)
+            self._model_train_step += 1
+
+
+    def _trainPolicyCriticFull(self, epochs):
+        """
+        TODO: Don't iterate over whole rb as the training time increases over time as rb expands
+        """
+        for i in range(epochs):
+            for exp in self._replay_buffer.sample_batch_full(self._minibatch_size):
+                current_state_batch, action_batch, reward_batch, termination_batch, next_state_batch = exp
+                current_state_batch = current_state_batch.reshape(self._minibatch_size, -1)
+                action_batch = action_batch.reshape(self._minibatch_size, -1)
+                reward_batch = reward_batch.reshape(self._minibatch_size, -1)
+                next_state_batch = next_state_batch.reshape(self._minibatch_size, -1)
+                # Calculate targets
+                target_q = self._value_estimator.predict_target(
+                    next_state_batch, self._policy_estimator.predict_target(next_state_batch))
+
+                y_i = []
+                for k in range(self._minibatch_size):
+                    if termination_batch[k]:
+                        y_i.append(reward_batch[k])
+                    else:
+                        y_i.append(reward_batch[k] + self._discount_factor * target_q[k])
+
+                # Update the critic given the targets
+                predicted_q_value, _, ve_loss = self._value_estimator.update(
+                    current_state_batch, action_batch, np.reshape(y_i, (self._minibatch_size, 1)))
+
+                max_qs.append(np.amax(predicted_q_value))
+
+                # Update the actor policy using the sampled gradient
+                a_outs = self._policy_estimator.predict(current_state_batch)
+                grads = self._value_estimator.action_gradients(current_state_batch, a_outs)
+                self._policy_estimator.update(current_state_batch, grads[0])
+
+                # Early stop
+                if np.isnan(ve_loss):
+                    logger.error("Training: value estimator loss is nan, stop training")
+                    self._stop_training = True
+
+                # Some basic summary of training loss
+                #if self._step % 100 == 0:
+                #    self._summary_writer.writeSummary({"ValueEstimatorTrainLoss": ve_loss}, self._step)
+
+            if i % 10 == 0:
+                # Update target networks
+                self._policy_estimator.update_target_network()
+                self._value_estimator.update_target_network()
+
+    def _generateRandomAction(self, max_vel):
+        """
+            Generate an array of shape (6,) of range [-max_vel, max_vel].
+        """
+        return np.array([random.random() * max_vel * 2 - max_vel for _ in range(6)])
+
+
+    def _getBestAction(self):
+        if not self._best_action_from_model:
+            return self._policy_estimator.predict(self._last_state) + self._actor_noise()
+        """
+        Assume reward function is given
+        """
+        # TODO: Remove explicit reward from explicit environment
+        from Environments.VREPEnvironments import VREPPushTaskEnvironment
+        best_action = None
+        max_horizon_reward = -float("inf")
+        current_state = np.copy(self._last_state)
+        for i in range(self._num_random_action):
+            horizon_reward = 0
+            first_action = None
+            for j in range(self._model_plan_horizon):
+                # TODO: Hard coded max velocity
+                action = self._generateRandomAction(1.0).reshape(1, -1)
+                if j == 0:
+                    first_action = action
+                next_state = current_state + self._model_estimator.predict(current_state, action)
+                horizon_reward += VREPPushTaskEnvironment.getRewards(current_state, action)
+                current_state = next_state
+            if best_action is None or horizon_reward > max_horizon_reward:
+                best_action = first_action
+                max_horizon_reward = horizon_reward
+        return best_action
+
+
+
+    def act(self, observation, last_reward, termination, episode_start_num, episode_num, episode_num_var, is_learning=False):
+        self._total_reward += last_reward
+        self._step += 1
+        # Putting a limit on how long the a single trial can run for simplicity
+        if self._step > self._max_episode_length:
+            termination = True
+
+        current_state = observation.reshape(1, -1)
+
+        # Initial step
+        if self._last_state is None:
+            # Train model full
+            self._trainModel(1000)
+            # Initialize the last state and action
+            self._last_state = current_state
+            best_action = self._getBestAction()
+            # TODO:
+            #self._best_action_from_model = not self._best_action_from_model
+            self._best_action_from_model = True
+            self._last_action = best_action
+            return best_action, termination
+
+        # Store the last step
+        self._replay_buffer.add(self._last_state.squeeze().copy(), self._last_action.squeeze().copy(), last_reward, termination,
+                current_state.squeeze().copy())
+
+        self._last_state = current_state.copy()
+
+        if is_learning and self._replay_buffer.size() >= self._minibatch_size and not self._stop_training:
+            self._train()
+
+        best_action = self._getBestAction()
+        # Switch between getting action from model and policy, but get from policy exclusively after episode
+        # _stop_best_action_from_model_episode
+        # TODO:
+        #if episode_num >= self._stop_best_action_from_model_episode:
+        #    self._best_action_from_model = False
+        #else:
+        #    self._best_action_from_model = not self._best_action_from_model
+        self._best_action_from_model = True
+
+        self._last_action = best_action
+
+        if termination:
+            # Record cumulative reward of trial
+            self._rewards_list.append(self._total_reward)
+            average = np.mean(self._rewards_list[-self._num_rewards_to_average:])
+
+            # Update episode number variable
+            self._sess.run(tf.assign(episode_num_var, episode_num))
+
+            # Check for improvements
+            if len(self._rewards_list) >= self._num_rewards_to_average and\
+                (self._best_average is None or self._best_average < average):
+                self._best_average = average
+                improve_str = '*'
+            else:
+                improve_str = ''
+
+            # Log the episode summary
+            log_string = "Episode {0:>5}/{1:>5} ({2:>5}/{3:>5} in this run), " +\
+                         "R {4:>9.3f}, Ave R {5:>9.3f} {7}, Ave Max Q {6:>9.3f}"
+
+            logger.info(log_string.format(episode_num, self._num_episodes + episode_start_num - 1,
+                episode_num - episode_start_num + 1,
+                self._num_episodes,
+                self._total_reward[0], average, self._max_q / float(self._step), improve_str))
+
+            # Checkpoint
+            if is_learning:
+                if improve_str == '*':
+                    logger.info("Saving best agent so far")
+                    self.save(self._estimator_dir, is_best=True, step=episode_num, write_meta_graph=False)
+                if (episode_num % self._recent_save_freq == 0 or episode_num >= self._num_episodes +\
+                    episode_start_num - 1):
+                    logger.info("Saving agent checkpoints")
+                    self.save(self._estimator_dir, step=episode_num, write_meta_graph=False)
+                # Save summary
+                self._summary_writer.writeSummary({
+                    "TotalReward": self._total_reward[0],
+                    "AverageMaxQ": self._max_q / float(self._step),
+                    "BestAverage": self._best_average,
+                    }, episode_num)
+            # Save replay buffer
+            if (not self._replay_buffer_save_dir is None) and \
+                (episode_num % self._replay_buffer_save_freq == 0 or episode_num >= self._num_episodes +\
+                episode_start_num - 1):
+                logger.info("Saving replay buffer")
+                self.saveReplayBuffer(self._replay_buffer_save_dir)
+
+            # Check for convergence
+            if self._last_average and average <= self._last_average:
+                self._num_non_imp_eps += 1
+            else:
+                self._num_non_imp_eps = 0
+            if self._num_non_imp_eps >= self._max_num_non_imp_eps:
+                logger.info("Agent not improving; stop training")
+                self._stop_training = True
+            self._last_average = average
+
+            # Reset for new episode
+            self._total_reward = 0.0
+            self._max_q = 0.0
+            self._step = 0
+            self._last_state = None
+            self._last_action = None
+
+        return best_action, termination
+
+    def _train(self):
+        max_qs = []
+        for _ in range(self._num_updates):
+            current_state_batch, action_batch, reward_batch, termination_batch, next_state_batch =\
+                    self._replay_buffer.sample_batch(self._minibatch_size)
+            current_state_batch = current_state_batch.reshape(self._minibatch_size, -1)
+            action_batch = action_batch.reshape(self._minibatch_size, -1)
+            reward_batch = reward_batch.reshape(self._minibatch_size, -1)
+            next_state_batch = next_state_batch.reshape(self._minibatch_size, -1)
+            # Calculate targets
+            target_q = self._value_estimator.predict_target(
+                next_state_batch, self._policy_estimator.predict_target(next_state_batch))
+
+            y_i = []
+            for k in range(self._minibatch_size):
+                if termination_batch[k]:
+                    y_i.append(reward_batch[k])
+                else:
+                    y_i.append(reward_batch[k] + self._discount_factor * target_q[k])
+
+            # Update the critic given the targets
+            predicted_q_value, _, ve_loss = self._value_estimator.update(
+                current_state_batch, action_batch, np.reshape(y_i, (self._minibatch_size, 1)))
+
+            max_qs.append(np.amax(predicted_q_value))
+
+            # Update the actor policy using the sampled gradient
+            a_outs = self._policy_estimator.predict(current_state_batch)
+            grads = self._value_estimator.action_gradients(current_state_batch, a_outs)
+            self._policy_estimator.update(current_state_batch, grads[0])
+
+            # Early stop
+            if np.isnan(ve_loss):
+                logger.error("Training: value estimator loss is nan, stop training")
+                self._stop_training = True
+
+            # Some basic summary of training loss
+            #if self._step % 100 == 0:
+            #    self._summary_writer.writeSummary({"ValueEstimatorTrainLoss": ve_loss}, self._step)
+
+        # Update target networks
+        self._policy_estimator.update_target_network()
+        self._value_estimator.update_target_network()
+
+        self._max_q += max(max_qs)
---
+++ src/DPGAC2WithMultiPModelAndDemoPEH2VEH2MEH2.py
@@ -0,0 +1,245 @@
+import argparse
+import collections
+import random
+from EnvironmentFactory import EnvironmentContext
+from Utils import SortedDisplayDict
+import copy
+import gym
+import itertools
+import numpy as np
+import os
+import tensorflow as tf
+#from tensorflow.python import debug as tf_debug
+from Estimators.DPGMultiPerceptronPolicyEstimator import DPGMultiPerceptronPolicyEstimator
+from Estimators.DPGMultiPerceptronValueEstimator import DPGMultiPerceptronValueEstimator
+from Estimators.MultiPerceptronModelEstimator import MultiPerceptronModelEstimator
+from Agents.DPGAC2Agent import DPGAC2WithMultiPModelAndDemoAgent
+from Utils import SummaryWriter
+from Utils import OrnsteinUhlenbeckActionNoise
+from Utils import getModuleLogger
+
+# Module logger
+logger = getModuleLogger(__name__)
+
+def MakeDPGAC2WithMultiPModelAndDemoPEH2VEH2MEH2(session, env, args):
+    # The number of states about the environment the agent can observe
+    observation_space_dim = env.observation_space.shape[0]
+    # The number of actions the agent can make
+    action_space_dim = env.action_space.shape[0]
+    assert(env.action_space.high[0] == -env.action_space.low[0])
+
+    # The shapes of the hidden layers of the policy estimator
+    pe_layer_shapes = [
+            max(1, int(args.pe_h1_multiplier * observation_space_dim)),
+            max(1, int(args.pe_h2_multiplier * observation_space_dim)),
+            ]
+
+    # This implementation assumes a particular architecture for value estimator
+    # The shapes of the hidden layers of the policy estimator
+    ve_layer_shapes = [
+            max(1, int(args.ve_h1_multiplier * observation_space_dim)),
+            max(1, int(args.ve_h2_multiplier * observation_space_dim)),
+            ]
+
+    me_layer_shapes = [
+            max(1, int(args.me_h1_multiplier * observation_space_dim)),
+            max(1, int(args.me_h2_multiplier * observation_space_dim)),
+            ]
+
+    policy_estimator = DPGMultiPerceptronPolicyEstimator(
+            sess=session,
+            state_dim=observation_space_dim,
+            action_dim=action_space_dim,
+            h_layer_shapes=pe_layer_shapes,
+            learning_rate=args.pe_learning_rate,
+            action_bound=env.action_space.high[0],
+            tau=args.tau,
+            minibatch_size=2**args.minibatch_size_log,
+            imitation_learning_rate=args.pe_imitation_learning_rate,
+            imitation_minibatch_size=2**args.pe_imitation_minibatch_size_log,
+            )
+
+    value_estimator = DPGMultiPerceptronValueEstimator(
+            sess=session,
+            state_dim=observation_space_dim,
+            action_dim=action_space_dim,
+            h_layer_shapes=ve_layer_shapes,
+            learning_rate=args.ve_learning_rate,
+            tau=args.tau,
+            num_actor_vars=policy_estimator.get_num_trainable_vars()
+            )
+
+    model_estimator = MultiPerceptronModelEstimator(
+            sess=session,
+            state_dim=observation_space_dim,
+            action_dim=action_space_dim,
+            h_layer_shapes=me_layer_shapes,
+            learning_rate=args.me_learning_rate
+            )
+
+    summary_writer = SummaryWriter(session, args.summary_dir,[
+        "TotalReward",
+        "AverageMaxQ",
+        "BestAverage",
+        #"ValueEstimatorTrainLoss"
+        ])
+
+    imitation_summary_writer = SummaryWriter(session, os.path.join(args.summary_dir, "ImitationLearning"),[
+        "TrainLoss",
+        ])
+
+    model_summary_writer = SummaryWriter(session, os.path.join(args.summary_dir, "ModelLearning"),[
+        "ModelTrainLoss",
+        ])
+
+    estimator_saver_recent = tf.train.Saver(max_to_keep=args.max_estimators_to_keep)
+    estimator_saver_best = tf.train.Saver(max_to_keep=1)
+
+    actor_noise = OrnsteinUhlenbeckActionNoise(mu=np.zeros(action_space_dim))
+
+    return DPGAC2WithMultiPModelAndDemoAgent(
+                sess=session,
+                policy_estimator=policy_estimator,
+                value_estimator=value_estimator,
+                model_estimator=model_estimator,
+                discount_factor=args.discount_factor,
+                num_episodes=args.num_episodes,
+                max_episode_length=args.max_episode_length,
+                minibatch_size=2**args.minibatch_size_log,
+                actor_noise=actor_noise,
+                replay_buffer_size=10**args.replay_buffer_size_log,
+                summary_writer=summary_writer,
+                imitation_summary_writer=imitation_summary_writer,
+                model_summary_writer=model_summary_writer,
+                estimator_dir=args.estimator_dir,
+                estimator_saver_recent=estimator_saver_recent,
+                estimator_saver_best=estimator_saver_best,
+                recent_save_freq=args.estimator_save_freq,
+                replay_buffer_save_dir=args.replay_buffer_save_dir,
+                replay_buffer_save_freq=args.replay_buffer_save_freq,
+                num_updates=args.num_updates,
+                )
+
+def getArgParser():
+    # Build argument parser
+    parser = argparse.ArgumentParser(description="provide arguments for DPGAC2PEH1VEH1 agent")
+
+    # Session parameters
+    parser.add_argument("--env-name", help="choose the env[VREPPushTask, Pendulum-v0]", required=True)
+    parser.add_argument("--estimator-dir", help="directory for loading/storing estimators", required=True)
+    parser.add_argument("--summary-dir", help="directory for storing tensorboard info", required=True)
+    #parser.add_argument("--agent-name", help="name of the agent")
+    parser.add_argument("--stop-agent-learning", help="Is Agent learning", action="store_true")
+    parser.add_argument("--num-episodes", help="max num of episodes to do while training", type=int, default=500)
+    parser.add_argument("--max-episode-length", help="max length of 1 episode", type=int, default=100)
+    parser.add_argument("--random-seed", help="random seed for repeatability", type=int, default=1234)
+    parser.add_argument("--render-env", help="render the env", action="store_true")
+    parser.add_argument("--new-estimator", help="if creating new estimators instead of loading old ones", action="store_true")
+    parser.add_argument("--max-estimators-to-keep", help="maximal number of estimators to keep checkpointing",
+            type=int, default=2)
+    parser.add_argument("--estimator-save-freq", help="estimator save frequency (per number of episodes)",
+            type=int, default=50)
+    parser.add_argument("--estimator-load-mode", help="0: load most recent 1: load best", type=int, default=0)
+    parser.add_argument("--replay-buffer-load-dir", help="directory for loading replay buffer")
+    parser.add_argument("--replay-buffer-save-dir", help="directory for storing replay buffer")
+    parser.add_argument("--replay-buffer-save-freq", help="replay buffer save frequency (per number of episodes", type=int,
+            default=500)
+
+    parser.set_defaults(stop_agent_learning=False)
+    parser.set_defaults(render_env=False)
+    parser.set_defaults(new_estimator=False)
+
+    # Add Agent parameters
+    parser.add_argument("--pe-learning-rate", help="Policy esitmator learning rate", type=float, default=0.0001)
+    parser.add_argument("--ve-learning-rate", help="Value esitmator learning rate", type=float, default=0.001)
+    parser.add_argument("--pe-h1-multiplier", help="Policy estimator hidden layer 1 size multiplier", type=float, default=10)
+    parser.add_argument("--ve-h1-multiplier", help="Value estimator hidden layer 1 size multiplier", type=float, default=10)
+    parser.add_argument("--pe-h2-multiplier", help="Policy estimator hidden layer 2 size multiplier", type=float, default=10)
+    parser.add_argument("--ve-h2-multiplier", help="Value estimator hidden layer 2 size multiplier", type=float, default=10)
+    parser.add_argument("--discount-factor", help="discount factor for critic updates", type=float, default=0.99)
+    parser.add_argument("--tau", help="soft target update parameter", type=float, default=0.001)
+    parser.add_argument("--minibatch-size-log", help="size of minibatch for minibatch-SGD as exponent of 2",
+            type=int, default=7)
+    parser.add_argument("--replay-buffer-size-log", help="max size of the replay buffer as exponent of 10", type=int, default=6)
+    parser.add_argument("--num-updates", help="number of estimator updates per training step", type=int, default=1)
+    parser.add_argument("--pe-imitation-learning-rate", help="Policy esitmator imitation learning rate", type=float, default=0.001)
+    parser.add_argument("--pe-imitation-minibatch-size-log", help="size of minibatch for imitation minibatch-SGD as exponent of 2",
+            type=int, default=7)
+    parser.add_argument("--me-learning-rate", help="Model esitmator learning rate", type=float, default=0.001)
+    parser.add_argument("--me-h1-multiplier", help="Model estimator hidden layer 1 size multiplier", type=float, default=10)
+    parser.add_argument("--me-h2-multiplier", help="Model estimator hidden layer 2 size multiplier", type=float, default=10)
+
+    return parser
+
+def runEnvironmentWithAgent(args):
+    # dirty but works
+    from AgentFactory import MakeAgent
+    logger.info("Run info:")
+    logger.info(SortedDisplayDict(vars(args)))
+    config = tf.ConfigProto()
+    config.gpu_options.allow_growth = True
+    config.log_device_placement = False
+    # Set graph-level random seed to ensure repeatability of experiments
+    tf.set_random_seed(args.random_seed)
+    random.seed(args.random_seed)
+    np.random.seed(args.random_seed)
+    logger.info("Making environment {}".format(args.env_name))
+    with EnvironmentContext(args.env_name) as env, tf.Session(config=config) as session:
+        # To record progress across different training sessions
+        global_episode_num = tf.Variable(0, name="global_episode_num", trainable=False)
+        logger.info("Making agent {}".format(args.agent_name))
+        agent = MakeAgent(session, env, args)
+
+        session.run(tf.global_variables_initializer())
+        if args.new_estimator:
+            logger.info("Saving session meta file to {}".format(args.estimator_dir))
+            agent.save(args.estimator_dir, step=0, write_meta_graph=True)
+        else:
+            logger.info("Restoring agent from {}".format(args.estimator_dir))
+            agent.load(args.estimator_dir, is_best=(args.estimator_load_mode==1))
+        if not args.replay_buffer_load_dir is None:
+            logger.info("Restoring replay buffer from {}".format(args.replay_buffer_load_dir))
+            agent.loadReplayBuffer(args.replay_buffer_load_dir)
+
+
+        episode_start = session.run(global_episode_num) + 1
+        logger.info("Continueing at episode {}".format(episode_start))
+        # Run the environment feedback loop
+        for episode_num in range(episode_start, episode_start + args.num_episodes):
+            observation = env.reset()
+            reward = 0.0
+            done = False
+            action, done = agent.act(observation, reward, done, episode_start, episode_num, global_episode_num,
+                    is_learning=(not args.stop_agent_learning))
+
+            while not done:
+                if args.render_env:
+                    env.render()
+                observation, reward, done, _ = env.step(action)
+                action, done = agent.act(observation, reward, done, episode_start, episode_num, global_episode_num,
+                                         is_learning=(not args.stop_agent_learning))
+                #logger.debug("Observation")
+                #logger.debug(observation)
+                #logger.debug("Action")
+                #logger.debug(action)
+                #logger.debug("Reward")
+                #logger.debug(reward)
+                #logger.debug("Done")
+                #logger.debug(done)
+            # No need to push forward when the agent stops training and has collected enough episodes to obtain a score
+            if agent._stop_training and agent.score():
+                logger.warn("Agent stopped training. Exiting experiment...")
+                break
+
+    logger.info("Best score: {}".format(agent.score()))
+    logger.info("Exiting environment: {}".format(args.env_name))
+    return agent.score()
+
+if __name__ == "__main__":
+
+    args = getArgParser().parse_args()
+    args.agent_name="DPGAC2WithMultiPModelAndDemoPEH2VEH2MEH2"
+
+    logger.info("Starting Agent {} in Environment {}".format(args.agent_name, args.env_name))
+    runEnvironmentWithAgent(args)
+    logger.info("Exiting")
