[n-step td fix patch1] Calculate uniform n-step td for a rollout and fix the relation between lambda2 and number of steps in n-step td calculation

src/Agents/DPGAC2Agent.py
@@ -930,6 +930,7 @@ class DPGAC2WithPrioritizedRB(AgentBase):
             current_state_batch = current_state_batch.reshape(self._minibatch_size, -1)
             action_batch = action_batch.reshape(self._minibatch_size, -1)
             reward_batch = reward_batch.reshape(self._minibatch_size, -1)
+            weights = weights.reshape(self._minibatch_size, -1)
             next_state_batch = next_state_batch.reshape(self._minibatch_size, -1)
 
             predicted_target_q = self._value_estimator.predict_target(
@@ -941,8 +942,12 @@ class DPGAC2WithPrioritizedRB(AgentBase):
                     td_target.append(reward_batch[k])
                 else:
                     td_target.append(reward_batch[k] + self._discount_factor * predicted_target_q[k])
+            td_target = np.reshape(td_target, (-1, 1))
 
             # Calculate n-step targets
+            #TODO: Remove hardcoded N
+            # Calculate 10-step targets
+            num_step = 10
             ns_current_state_batch, ns_action_batch, ns_reward_batch, ns_next_state_batch, ns_termination_batch,\
                     ns_weights, ns_indexes =\
                         self._replay_buffer.sample_episode(self._replay_buffer_beta)
@@ -950,43 +955,50 @@ class DPGAC2WithPrioritizedRB(AgentBase):
             ns_action_batch = ns_action_batch.reshape(-1, self._value_estimator._action_dim)
             ns_reward_batch = ns_reward_batch.reshape(-1, 1)
             ns_next_state_batch = ns_next_state_batch.reshape(-1, self._value_estimator._state_dim)
+            ns_weights = ns_weights.reshape(-1, 1)
+            rollout_length = ns_next_state_batch.shape[0]
+            assert(rollout_length > 0)
 
             #TODO: Use action from the last transition instead? Since it's already available. Investigate
-            ns_td_target = []
-            if ns_next_state_batch.shape[0] > 1:
-                ns_predicted_last_target_q = self._value_estimator.predict_target(
-                        ns_next_state_batch[-2, :].reshape(1, -1), self._policy_estimator.predict_target(ns_next_state_batch[-2, :].reshape(1, -1)))
-
+            # Only calculate n1mix target when the sampled rollout contains greater than num_step number of transitions
+            if rollout_length > num_step:
+                # Initialise ns_td_target
                 # Note that start bootstrapping from the second-to-last transition, ie use estimate on the last 'current
                 # state' and use pure reward for the last transition
-                last_td_target = ns_predicted_last_target_q
-                for k in range(ns_current_state_batch.shape[0] - 1, -1, -1):
-                    if ns_termination_batch[k]:
-                        ns_td_target.insert(0, ns_reward_batch[k])
-                    else:
-                        last_td_target = ns_reward_batch[k] + self._discount_factor * last_td_target
-                        ns_td_target.insert(0, last_td_target)
-            elif ns_next_state_batch.shape[0] == 1:
-                ns_td_target.append(ns_reward_batch[0])
+                nb_ns_td_target = rollout_length - num_step + 1
+                ns_td_target = np.zeros((nb_ns_td_target, 1))
+                ns_td_target[:-1] = self._value_estimator.predict_target(
+                        ns_next_state_batch[num_step-1:rollout_length-1],
+                        self._policy_estimator.predict_target(ns_next_state_batch[num_step-1:rollout_length-1]))
+                #print("NS_TD_TARGET")
+                #print(ns_td_target)
+                for k in range(num_step):
+                    ns_td_target = ns_reward_batch[num_step-1-k:rollout_length-k] + self._discount_factor * ns_td_target
+                # Combine 1-step batches with n-step batches
+                current_state_batch = np.concatenate([current_state_batch, ns_current_state_batch[:nb_ns_td_target, :]], axis=0)
+                action_batch = np.concatenate([action_batch, ns_action_batch[:nb_ns_td_target, :]], axis=0)
+                td_target = np.concatenate([td_target, ns_td_target], axis=0)
+                weights = np.concatenate([weights, ns_weights[:nb_ns_td_target, :]], axis=0)
+                indexes = indexes + ns_indexes[:nb_ns_td_target]
             else:
-                raise ValueError("Empty episode batch")
+                nb_ns_td_target = 0
 
-            # Combine 1-step batches with n-step batches
-            current_state_batch = np.concatenate([current_state_batch, ns_current_state_batch], axis=0)
-            action_batch = np.concatenate([action_batch, ns_action_batch], axis=0)
-            td_target = td_target + ns_td_target
-            weights = np.concatenate([weights, ns_weights], axis=0)
-            indexes = indexes + ns_indexes
             #print("WEIGHTS")
             #print(weights)
             #print("INDEXES")
             #print(indexes)
             #print("TD_TARGET")
             #print(td_target)
+            #print("ROLLOUT_LENGTH")
+            #print(rollout_length)
+            #print(current_state_batch.shape[0])
+            #print(action_batch.shape[0])
+            #print(td_target.shape[0])
+            #print(weights.shape[0])
+            #print(len(indexes))
             # Update the critic given the targets with weights
             _, td_error, ve_weighted_loss, ve_loss = self._value_estimator.update_with_weights_and_n1s_td(
-                current_state_batch, action_batch, np.reshape(td_target, (-1, 1)),
-                weights.reshape(-1, 1), ns_current_state_batch.shape[0])
+                current_state_batch, action_batch, td_target, weights, rollout_length)
             self._stats_epoch_critic_loss.append(ve_weighted_loss)
 
             # NB: Use td_target because it's not pure estimate (reward as samples)

src/Estimators/DPGMultiPerceptronValueEstimator.py
@@ -14,7 +14,7 @@ class DPGMultiPerceptronValueEstimator(object):
     """
 
     def __init__(self, sess, state_rms, return_rms, state_dim, action_dim, h_layer_shapes, state_range, return_range,
-            learning_rate, tau, num_actor_vars, lambda2=0.5,
+            learning_rate, tau, num_actor_vars, lambda2=0.1,
             scope="value_estimator"):
         self._sess = sess
         self._state_dim = state_dim
@@ -25,6 +25,8 @@ class DPGMultiPerceptronValueEstimator(object):
         self._learning_rate = learning_rate
         self._tau = tau
         # Weights of the n-step term in the loss function
+        # TODO: remove hardcoded.
+        # NB: lambda2 = 1/num_steps for n-step td calculation
         self._lambda2 = lambda2
 
         self._state_rms = state_rms
@@ -83,10 +85,11 @@ class DPGMultiPerceptronValueEstimator(object):
             # Weighted n-1 step update
             # The loss function is defined in the paper [Leverage...]
             self._nb_ns_td_target = tf.placeholder(tf.int32, [], name="nb_ns_td_target")
-            self._n1s_loss = tf.reduce_mean(td_error_sq[:-self._nb_ns_td_target])\
-                    + self._lambda2 * tf.reduce_mean(td_error_sq[-self._nb_ns_td_target:])
-            self._weighted_n1s_loss = tf.reduce_mean(weighted_td_error_sq[:-self._nb_ns_td_target])\
-                    + self._lambda2 * tf.reduce_mean(weighted_td_error_sq[-self._nb_ns_td_target:])
+            mid_indx = tf.shape(td_error_sq)[0] - self._nb_ns_td_target
+            self._n1s_loss = tf.reduce_mean(td_error_sq[:mid_indx])\
+                    + self._lambda2 * tf.reduce_mean(td_error_sq[mid_indx:])
+            self._weighted_n1s_loss = tf.reduce_mean(weighted_td_error_sq[:mid_indx])\
+                    + self._lambda2 * tf.reduce_mean(weighted_td_error_sq[mid_indx:])
             self._weighted_n1s_optimize = opt.minimize(self._weighted_n1s_loss, var_list=self._network_params, name="weighted_n1s_optimize")
 
             # Get the gradient of the net w.r.t. the action.